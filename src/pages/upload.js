5999a753f41d5b5da1fc43d5b5e15e55de9a5921
Matthew Lo <Matt@Matthews-MacBook-Pro-63.local>
2017-11-01 15:39:26 -0700

    Updated upload.js

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -3,12 +3,31 @@ import {connect} from 'react-redux';
 import Store from '../stores/index';
 
 class Upload extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0
-    };
+  constructor(props) {
+    super(props);
+    this.state = {file: ' ', imagePreviewUrl: ''};
   }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('handling uploading-', this.state.file);
+  }
+
+  changeImage(e){
+    e.preventDefault();
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
 
   onClick() {
     console.log('clicked');
@@ -19,14 +38,60 @@ class Upload extends React.Component {
 
   render() {
     console.log('props', window.__APP_INITIAL_STATE__);
+
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src = {imagePreviewUrl} />);
+    }
+    else{
+      $imagePreview = (<div className = "noImageText"> Please select an image</div>);
+    }
+
+
+    const buttonStyle = {
+      height: 50,
+      width: 300,
+      backgroundColor:'#00BFFF',
+      borderColor:'#00BFFF',
+      alignSelf: 'center',
+      justifyContent:'center',
+      fontSize: '22px',
+      color: 'white'
+    };
+
+
+    const headerStyle = {
+      textAlign: 'center',
+      fontSize: '40px',
+      color: 'white'
+    };
+
     return (
+      <body style = {{marginTop: '0', paddingTop: '0', textAlign: 'center'}}>
+
       <div>
-        <div style={{width: 10, height: 10, background: 'red'}} onClick={() => {this.onClick()}}/>
-        <p>
-          This is an upload page, {this.state.counter}
-          Another counter {Store.getState()}
-        </p>
+      <p>
+      <h1 style = {headerStyle}>
+        Upload an image that you want your friends to edit!
+      </h1>
+      </p>
       </div>
+
+      <div className = "previewComponent">
+        <form onSubmit = {(e) => this.handleSubmit(e)}>
+          <input className = "imgInput"
+            type = "file" onChange = {(e) => this.changeImage(e)} />
+          
+          <button style = {buttonStyle}>Upload an image!</button>
+        </form>
+
+        <div className = "imgPreview" style = {{margin: '10px', padding: '10px'}}>
+          {$imagePreview}
+        </div>
+      </div>
+      
+      </body>
     );
   }
 }
